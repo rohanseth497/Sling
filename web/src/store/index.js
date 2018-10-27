a66a864619d5ca153e63e9d63b68abe35f4de00c
zorail <rohan.seth497@gmail.com>
2018-10-28 00:04:57 +0530

    created reducer and store

---
+++ web/src/store/index.js
@@ -0,0 +1,12 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import reducers from '../reducers';
+
+const middleware = [thunk];
+
+const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore);
+
+const store = createStoreWithMiddleware(reducers);
+
+export default store;
