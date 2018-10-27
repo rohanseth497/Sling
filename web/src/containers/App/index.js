1c966313727d8b754eddb15e8ca945f6723260e1
zorail <rohan.seth497@gmail.com>
2018-10-28 00:43:17 +0530

    react router setup done

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import NotFound from '../NotFound';
 
 const App = () => (
   <Router>
-    <div>
-      <Route exactly path="/" component={Home} />
+    <Switch>
+      <Route exact path="/" component={Home} />
       <Route path="*" component={NotFound} />
-    </div>
+    </Switch>
   </Router>
 );
 
