4e86242a9eb89528b28b940b34a9de386e1e0e47
zorail <rohan.seth497@gmail.com>
2018-10-28 03:06:04 +0530

    made session view

--- api/web/router.ex
+++ api/web/router.ex
@@ -3,9 +3,13 @@ defmodule Sling.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource
   end
 
   scope "/api", Sling do
     pipe_through :api
+
+    # post "/sessions",
   end
 end
--- api/web/views/user_view.ex
+++ api/web/views/user_view.ex
@@ -1,18 +1,17 @@
 defmodule Sling.UserView do
   use Sling.Web, :view
 
-  def render("index.json", %{users: users}) do
-    %{data: render_many(users, Sling.UserView, "user.json")}
-  end
+  # def render("index.json", %{users: users}) do
+  #   %{data: render_many(users, Sling.UserView, "user.json")}
+  # end
 
-  def render("show.json", %{user: user}) do
-    %{data: render_one(user, Sling.UserView, "user.json")}
-  end
+  # def render("show.json", %{user: user}) do
+  #   %{data: render_one(user, Sling.UserView, "user.json")}
+  # end
 
   def render("user.json", %{user: user}) do
     %{id: user.id,
       username: user.username,
-      email: user.email,
-      password_hash: user.password_hash}
+      email: user.email}
   end
 end
