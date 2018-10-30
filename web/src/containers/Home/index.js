d6ec28d05eef41f39f67e35e667746095363b7de
zorail <rohan.seth497@gmail.com>
2018-10-30 17:45:37 +0530

    Home component done

--- web/src/containers/Home/index.js
+++ web/src/containers/Home/index.js
@@ -1,5 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/Session';
+import Navbar from '../../components/Navbar';
 
-const Home = () => <div>Home</div>;
+class Home extends React.Component {
 
-export default Home;
+  
+  handleLogout = () => this.props.logout()
+
+  render() {
+    const { currentUser, isAuthenticated } = this.props;
+
+    return (
+      <div style={{ flex: '1' }}>
+        <Navbar />
+        <ul>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/signup">Signup</Link></li>
+        </ul>
+        {isAuthenticated &&
+          <div>
+            <span>{currentUser.username}</span>
+            <button type="button" onClick={this.handleLogout}>Logout</button>
+          </div>
+        }
+      </div>
+    );
+  }
+}
+
+Home.propTypes = {
+  logout: PropTypes.func.isRequired,
+  currentUser: PropTypes.object,
+  isAuthenticated: PropTypes.bool,
+}
+
+export default connect(
+  state => ({
+    isAuthenticated: state.session.isAuthenticated,
+    currentUser: state.session.currentUser
+  }),
+  { logout }
+)(Home);
