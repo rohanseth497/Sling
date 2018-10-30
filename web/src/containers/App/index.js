0a6ab5850324e8ccef0a271fbd647696ef0808ce
zorail <rohan.seth497@gmail.com>
2018-10-30 09:36:34 +0530

    signup form component

--- web/src/containers/App/index.js
+++ web/src/containers/App/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from '../Home';
 import NotFound from '../NotFound';
+import Login from '';
+import Signup from '';
 
 const App = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
       <Route path="*" component={NotFound} />
     </Switch>
   </Router>
