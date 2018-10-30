be9306a0ad9dfcc3534ec71f6632edf5b07a21c0
zorail <rohan.seth497@gmail.com>
2018-10-30 10:15:12 +0530

    login form component

--- web/src/components/LoginForm/index.js
+++ web/src/components/LoginForm/index.js
@@ -1,10 +1,8 @@
-// @flow
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 import { css, StyleSheet } from 'aphrodite';
 import Input from '../Input';
-
 
 const styles = StyleSheet.create({
   card: {
@@ -16,11 +14,11 @@ const styles = StyleSheet.create({
 
 type Props = {
   onSubmit: () => void,
-  submitting: boolean,
   handleSubmit: () => void,
+  submitting: boolean,
 }
 
-class SignupForm extends React.Component {
+class LoginForm extends Component {
   props: Props
 
   handleSubmit = data => this.props.onSubmit(data);
@@ -33,38 +31,19 @@ class SignupForm extends React.Component {
         className={`card ${css(styles.card)}`}
         onSubmit={handleSubmit(this.handleSubmit)}
       >
-        <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Create an account</h3>
-        <Field
-          name="username"
-          type="text"
-          component={Input}
-          placeholder="Username"
-          className="form-control"
-        />
-        <Field
-          name="email"
-          type="email"
-          component={Input}
-          placeholder="Email"
-          className="form-control"
-        />
-        <Field
-          name="password"
-          type="password"
-          component={Input}
-          placeholder="Password"
-          className="form-control"
-        />
+        <h3 style={{ marginBottom: '2rem', textAlign: 'center' }}>Login to Sling</h3>
+        <Field name="email" type="text" component={Input} placeholder="Email" />
+        <Field name="password" type="password" component={Input} placeholder="Password" />
         <button
           type="submit"
           disabled={submitting}
           className="btn btn-block btn-primary"
         >
-          {submitting ? 'Submitting...' : 'Sign up'}
+          {submitting ? 'Logging in...' : 'Login'}
         </button>
         <hr style={{ margin: '2rem 0' }} />
-        <Link to="/login" className="btn btn-block btn-secondary">
-          Login to your account
+        <Link to="/signup" className="btn btn-block btn-secondary">
+          Create a new account
         </Link>
       </form>
     );
@@ -73,21 +52,16 @@ class SignupForm extends React.Component {
 
 const validate = (values) => {
   const errors = {};
-  if (!values.username) {
-    errors.username = 'Required';
-  }
   if (!values.email) {
     errors.email = 'Required';
   }
   if (!values.password) {
     errors.password = 'Required';
-  } else if (values.password.length < 6) {
-    errors.password = 'Minimum of 6 characters';
   }
   return errors;
 };
 
 export default reduxForm({
-  form: 'signup',
+  form: 'login',
   validate,
-})(SignupForm);
+})(LoginForm);
