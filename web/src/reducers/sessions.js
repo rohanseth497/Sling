c104a3270be864c15e26214b97d8b63e58473535
zorail <rohan.seth497@gmail.com>
2018-10-31 22:10:38 +0530

    session reducer updated

--- web/src/reducers/sessions.js
+++ web/src/reducers/sessions.js
@@ -1,22 +1,40 @@
-import { AUTHENTICATION_SUCCESS, LOGOUT } from '../actions/action_types';
+import {
+  AUTHENTICATION_SUCCESS,
+  AUTHENTICATION_REQUEST,
+  AUTHENTICATION_FAILURE,
+  LOGOUT,
+} from '../actions/action_types';
 
 const initialState = {
   isAuthenticated: false,
+  willAuthenticate: true,
   currentUser: {},
 };
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case AUTHENTICATION_REQUEST:
+      return {
+        ...state,
+        willAuthenticate: true,
+      };
     case AUTHENTICATION_SUCCESS:
       return {
         ...state,
+        willAuthenticate: false,
         isAuthenticated: true,
         currentUser: action.response.data,
+      };
+    case AUTHENTICATION_FAILURE:
+      return {
+        ...state,
+        willAuthenticate: false,
       };
     case LOGOUT:
       return {
         ...state,
         isAuthenticated: false,
+        willAuthenticate: false,
         currentUser: {},
       };
     default:
