ee69dd454df4c0b4568f99c19a7f416217df74fd
zorail <rohan.seth497@gmail.com>
2018-11-03 22:21:50 +0530

    room controller

--- api/web/controllers/api/room_controller.ex
+++ api/web/controllers/api/room_controller.ex
@@ -2,54 +2,54 @@ defmodule Sling.RoomController do
   use Sling.Web, :controller
 
   alias Sling.Room
+  alias Sling.UserRoom
+  alias Sling.Auth.Guardian
 
   def index(conn, _params) do
     rooms = Repo.all(Room)
     render(conn, "index.json", rooms: rooms)
   end
 
-  def create(conn, %{"room" => room_params}) do
-    changeset = Room.changeset(%Room{}, room_params)
+  def create(conn, params) do
+    current_user = Guardian.Plug.current_resource(conn)
+    changeset = Room.changeset(%Room{}, params)
 
     case Repo.insert(changeset) do
       {:ok, room} ->
+        assoc_changeset = UserRoom.changeset(
+          %UserRoom{},
+          %{user_id: current_user.id, room_id: room.id}
+        )
+        Repo.insert(assoc_changeset)
+
         conn
         |> put_status(:created)
-        # |> put_resp_header("location", room_path(conn, :show, room))
         |> render("show.json", room: room)
       {:error, changeset} ->
         conn
         |> put_status(:unprocessable_entity)
         |> render(Sling.ChangesetView, "error.json", changeset: changeset)
+
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    room = Repo.get!(Room, id)
-    render(conn, "show.json", room: room)
-  end
+  def join(conn, %{"id" => room_id}) do
+    current_user = Guardian.Plug.current_resource(conn)
+    room = Repo.get(Room, room_id)
 
-  def update(conn, %{"id" => id, "room" => room_params}) do
-    room = Repo.get!(Room, id)
-    changeset = Room.changeset(room, room_params)
+    changeset = UserRoom.changeset(
+      %UserRoom{},
+      %{room_id: room.id, user_id: current_user.id}
+    )
 
-    case Repo.update(changeset) do
-      {:ok, room} ->
-        render(conn, "show.json", room: room)
-      {:error, changeset} ->
+    case Repo.insert(changeset) do
+      {:ok, _user_room} ->
         conn
+        |> put_status(:created)
+        |> render("show.json", %{room: room})
+      {:error, changeset} ->
         |> put_status(:unprocessable_entity)
         |> render(Sling.ChangesetView, "error.json", changeset: changeset)
     end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    room = Repo.get!(Room, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(room)
-
-    send_resp(conn, :no_content, "")
   end
 end
