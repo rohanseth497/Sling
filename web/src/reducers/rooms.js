205abd5376f8f005a1ac01aa7b21d2d59043d5ab
zorail <rohan.seth497@gmail.com>
2018-11-03 22:47:03 +0530

    rooms reducer done

---
+++ web/src/reducers/rooms.js
@@ -0,0 +1,48 @@
+import {
+  FETCH_ROOMS_SUCCESS,
+  FETCH_USER_ROOMS_SUCCESS,
+  CREATE_ROOM_SUCCESS,
+  ROOM_JOINED,
+} from '../actions/action_types';
+
+const initialState = {
+  all: [],
+  currentUserRooms: [],
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case FETCH_ROOMS_SUCCESS:
+      return {
+        ...state,
+        all: action.response.data,
+      };
+    case FETCH_USER_ROOMS_SUCCESS:
+      return {
+        ...state,
+        currentUserRooms: action.response.data,
+      };
+    case CREATE_ROOM_SUCCESS:
+      return {
+        ...state,
+        all: [
+          action.response.data,
+          ...state.all,
+        ],
+        currentUserRooms: [
+          ...state.currentUserRooms,
+          action.response.data,
+        ],
+      };
+    case ROOM_JOINED:
+      return {
+        ...state,
+        currentUserRooms: [
+          ...state.currentUserRooms,
+          action.response.data,
+        ],
+      };
+    default:
+      return state;
+  }
+}
