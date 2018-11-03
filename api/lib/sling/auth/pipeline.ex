fa5701339d58d54750f76ea817cd5f30b3210a3e
zorail <rohan.seth497@gmail.com>
2018-11-03 12:28:42 +0530

    fixed server side routing

--- api/lib/sling/auth/pipeline.ex
+++ api/lib/sling/auth/pipeline.ex
@@ -5,12 +5,12 @@ defmodule Sling.Auth.Pipeline do
     error_handler: Sling.Auth.ErrorHandler
 
 
-    # plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-    # plug Guardian.Plug.LoadResource, allow_blank: true
+    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
+    plug Guardian.Plug.LoadResource, allow_blank: true
 
   # plug Guardian.Plug.VerifySession, claims: %{"typ" => "access"}
   # If there is an authorization header, restrict it to an access token and validate it
   # plug Guardian.Plug.VerifyHeader, claims: %{"typ" => "access"}, realm: "Bearer"
   # Load the user if either of the verifications worked
-  # plug Guardian.Plug.LoadResource, allow_blank: true
+    plug Guardian.Plug.LoadResource, allow_blank: true
 end
--- api/web/router.ex
+++ api/web/router.ex
@@ -4,19 +4,14 @@ defmodule Sling.Router do
   # alias Sling.Auth.Guardian
   pipeline :api do
     plug :accepts, ["json"]
-
-    plug Guardian.Plug.VerifyHeader, realm: "Bearer"
-    plug Guardian.Plug.LoadResource, allow_blank: true
     plug Sling.Auth.Pipeline
   end
 
   pipeline :guest_pipeline do
     plug Guardian.Plug.LoadResource, allow_blank: true
-    plug Sling.Auth.Pipeline
   end
 
   pipeline :login_required do
-    plug Sling.Auth.Pipeline
     plug Guardian.Plug.EnsureAuthenticated
   end
 
@@ -24,11 +19,13 @@ defmodule Sling.Router do
     pipe_through [:guest_pipeline]
     post "/users", UserController, :create
     post "/sessions", SessionController, :create
+
+    # scope "/" do
+      pipe_through [:api, :login_required]
+      delete "/sessions", SessionController, :delete
+      post "/sessions/refresh", SessionController, :refresh
+    # end
   end
 
-  scope "/api" do
-    pipe_through [:api, :login_required]
-    delete "/sessions", SessionController, :delete
-    post "/sessions/refresh", SessionController, :refresh
-  end
+
 end
