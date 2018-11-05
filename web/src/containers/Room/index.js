cefc9dd7305ed276d0473334584ab77457293f4b
zorail <rohan.seth497@gmail.com>
2018-11-05 21:17:02 +0530

    socket connection for a particular room

--- web/src/containers/Room/index.js
+++ web/src/containers/Room/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { connectToChannel, leaveChannel } from '../../actions/room';
 
-const Room = ({ match }) => (
-  <div>
-    Room
-    {match.params.id}
-  </div>
-);
+// const Room = ({ match }) => (
+//   <div>
+//     Room
+//     {match.params.id}
+//   </div>
+// );
 
-export default Room;
+class Room extends React.Component {
+  componentDidMount() {
+    const { userConnectToChannel, socket, match } = this.props;
+    userConnectToChannel(socket, match.params.id);
+  }
+
+  componentWillUnmount() {
+    const { userLeaveChannel, channel } = this.props;
+    userLeaveChannel(channel);
+  }
+
+  render() {
+    const { room } = this.props;
+    return (
+      <div>
+        {room.name}
+      </div>
+    );
+  }
+}
+
+Room.defaultProps = {
+  socket: {},
+  channel: {},
+  room: {},
+  match: {},
+};
+
+Room.propTypes = {
+  socket: PropTypes.instanceOf(Object),
+  channel: PropTypes.instanceOf(Object),
+  room: PropTypes.instanceOf(Object),
+  match: PropTypes.instanceOf(Object),
+  userConnectToChannel: PropTypes.func.isRequired,
+  userLeaveChannel: PropTypes.func.isRequired,
+};
+
+export default withRouter(connect(
+  state => ({
+    room: state.room.currentRoom,
+    socket: state.session.socket,
+    channel: state.room.channel,
+  }),
+  {
+    userConnectToChannel: connectToChannel,
+    userLeaveChannel: leaveChannel,
+  },
+)(Room));
--- web/src/reducers/room.js
+++ web/src/reducers/room.js
@@ -21,4 +21,4 @@ export default function (state = initialState, action) {
     default:
       return state;
   }
-};
+}
