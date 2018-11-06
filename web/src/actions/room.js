0b47bac0778145a531ddf572f2e43e1b7607c06f
zorail <rohan.seth497@gmail.com>
2018-11-07 03:46:16 +0530

    room action with presence actions

--- web/src/actions/room.js
+++ web/src/actions/room.js
@@ -1,14 +1,35 @@
 import { reset } from 'redux-form';
+import { Presence } from 'phoenix';
 import {
   ROOM_CONNECTED_TO_CHANNEL,
   USER_LEFT_ROOM,
   MESSAGE_CREATED,
+  ROOM_PRESENCE_UPDATE,
 } from './action_types';
+
+const syncPresentUsers = (dispatch, presences) => {
+  const presentUsers = [];
+  Presence.list(presences, (id, { metas: [first] }) => first.user)
+    .map(user => presentUsers.push(user));
+
+  dispatch({ type: ROOM_PRESENCE_UPDATE, presentUsers });
+};
 
 export const connectToChannel = (socket, roomId) => {
   return (dispatch) => {
     if (!socket) { return false; }
     const channel = socket.channel(`rooms:${roomId}`);
+    let presences = {};
+
+    channel.on('presence_state', (state) => {
+      presences = Presence.syncState(presences, state);
+      syncPresentUsers(dispatch, presences);
+    });
+
+    channel.on('presence_diff', (diff) => {
+      presences = Presence.syncDiff(presences, diff);
+      syncPresentUsers(dispatch, presences);
+    });
 
     channel.on('message_created', (message) => {
       dispatch({ type: MESSAGE_CREATED, message });
