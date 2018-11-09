9d32808967278c7c8e14e4b86d0683462a68f133
zorail <rohan.seth497@gmail.com>
2018-11-09 10:04:08 +0530

    added stylesheets and fonts

--- web/src/index.js
+++ web/src/index.js
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import store from './store';
 import App from './containers/App';
-
+import './styles/bootstrap.css';
+import './styles/font-awesome.css';
+import './styles/index.css';
 
 ReactDOM.render(
   <Provider store={store}>
